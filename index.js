3828c1194e7846278f3bbb8cea234f8dabedd013
lachimek <ExPL1234>
2020-08-31 20:27:39 +0200

    added procfile for heroku

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 
 const deadline = 'September 18 2020 22:00:00 GMT+0200';
+const sources = {
+    days: "750058655507939350",
+    hours: "750058667822285022",
+    minutes: "750058677775368240"
+};
 
 function getTimeRemaining(endTime){
     const total = Date.parse(endTime) - Date.parse(new Date());
@@ -22,16 +27,21 @@ function getTimeRemaining(endTime){
 
 client.on('ready', ()=>{
     console.log(`Zalogowano jako ${client.user.tag}`);
-    const channel = client.channels.cache.get(process.env.POE_CHANNEL_ID);
-    const startMsg = channel.send("placeholder");
-
+    const targets = {
+        days: client.channels.cache.get(sources.days),
+        hours: client.channels.cache.get(sources.hours),
+        minutes: client.channels.cache.get(sources.minutes),
+    };
     const timeInterval = setInterval(()=>{
         const {total, days, hours, minutes} = getTimeRemaining(process.env.DEADLINE_DATE || deadline);
-        startMsg.then(msg =>{
-            msg.edit(`Do startu ligi **Expedition** zostało: ${days}D ${hours}H ${minutes}M`);
-        });
+
+        targets.days.setName(`${days} - ${days < 2 && days !== 0 ? 'dzień' : 'dni'}`);
+        targets.hours.setName(`${hours} - ${hours < 2 && hours !== 0 ? 'godzina' : 'godzin'}`);
+        targets.minutes.setName(`${minutes} - ${minutes < 2 && minutes !== 0 ? 'minuta' : 'minut'}`);
+
         if(total <= 0) clearInterval(timeInterval);
     }, process.env.MSG_TIMEOUT || 2000);
+
 })
 
 client.login(process.env.BOT_TOKEN);
