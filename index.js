31514959e6d611225d40e006cb83b792d1581d34
lachimek <ExPL1234>
2020-09-08 19:41:04 +0200

    poelab integration

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const request = require('request-promise')
+const xpath = require('xpath')
+const dom = require('xmldom').DOMParser
 const Discord = require('discord.js')
 const client = new Discord.Client();
 
@@ -20,10 +23,23 @@ function getTimeRemaining(endTime){
     };
 }
 
+const poeLabUrl = 'https://www.poelab.com/';
+const absolutePath = '/html/body/div[1]/div[2]/div[1]/div[3]/div/div/center/div/';
+const lab = {
+    uber: `${absolutePath}div[1]/div/h2/a`,
+    merc: `${absolutePath}div[2]/div/h2/a`,
+    cruel: `${absolutePath}div[3]/div/h2/a`,
+    normal: `${absolutePath}div[4]/div/h2/a`,
+    img: `/html/body/div[1]/div[2]/div/div/article/div[3]/div[2]/span/img`,
+}
+
+const labOptions = ['uber', 'merc', 'cruel', 'normal'];
+
 client.on('ready', ()=>{
     console.log(`Zalogowano jako ${client.user.tag}`);
+    client.user.setPresence({activity: {name: 'Prefix komend: &'}}).then(r => console.log("ustawiono status"))
 
-    const timeInterval = setInterval(()=>{
+     const timeInterval = setInterval(()=>{
         const {total, days, hours, minutes} = getTimeRemaining(process.env.DEADLINE_DATE || deadline);
 
         const msg = `${days}d : ${hours}h : ${minutes}m`
@@ -34,6 +50,49 @@ client.on('ready', ()=>{
         if(total <= 0) clearInterval(timeInterval);
     }, process.env.MSG_TIMEOUT || 60000);
 
+})
+
+client.on('message', (message)=>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith('&')) return;
+
+    const command = message.content.split(' ');
+    if(command[0].toLowerCase().substr(1) === 'lab'){
+        let labType;
+        switch (command[1].toLowerCase()) {
+            case 'uber':
+                labType = lab.uber;
+                break;
+            case 'merc':
+                labType = lab.merc;
+                break;
+            case 'cruel':
+                labType = lab.cruel;
+                break;
+            case 'normal':
+                labType = lab.normal;
+                break;
+            default:
+                message.channel.send('Opcje: uber, merc, cruel, normal');
+        }
+        if(typeof labType !== 'undefined'){
+            request(poeLabUrl).then((data)=>{
+                console.log(`Lab type: ${labType}`);
+                const doc = new dom().parseFromString(data);
+                const nodes = xpath.select(labType, doc);
+                const href = nodes[0].attributes[1].value;
+                request(href).then((data)=>{
+                    const doc = new dom().parseFromString(data);
+                    const nodes = xpath.select(lab.img, doc);
+
+                    console.log(nodes[0].attributes[2].value);
+                    message.channel.send(`Tak wygląda dziś ${command[1].toLowerCase()} lab`, {files: [nodes[0].attributes[2].value]});
+                }).catch(err => console.log(err));
+            });
+        }
+    }else {
+        message.channel.send(`Dostępne komendy: &lab`);
+    }
 })
 
 client.on('rateLimit', (info) => {
